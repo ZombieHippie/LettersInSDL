d489015426086cc127f49a55d53fbd9bc40ba3d4
Cole R Lawrence <cole@zombiehippie.com>
2015-09-22 17:13:20 -0500

    Update compile.bat to work with

--- compile.bat
+++ compile.bat
@@ -1,1 +1,3 @@
-g++ Project1.cpp -IC:\mingw64\x86_64-w64-mingw32\include\SDL2 -LC:\mingw64\x86_64-w64-mingw32\lib -w -Wl,-subsystem,windows -lmingw32 -lSDL2main -lSDL2 -o Project1
+echo. & echo. & echo. & echo.
+g++ Project1.cpp -std=c++11 -IC:\mingw64\x86_64-w64-mingw32\include\SDL2 -LC:\mingw64\x86_64-w64-mingw32\lib -w -Wl,-subsystem,windows -lmingw32 -lSDL2main -lSDL2 -o ./dist/Project1.exe
+::"./dist-cole/Project1.exe"
