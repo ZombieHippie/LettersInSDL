e0ec933d6f9f7bbcbe663b0bc6f9338ba88479d9
Cole R Lawrence <cole@zombiehippie.com>
2015-09-27 12:36:01 -0500

    Remove subsystem requirementsto see console log

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
 echo. & echo. & echo. & echo.
-g++ Project1.cpp -std=c++11 -IC:\mingw64\x86_64-w64-mingw32\include\SDL2 -LC:\mingw64\x86_64-w64-mingw32\lib -w -Wl,-subsystem,windows -lmingw32 -lSDL2main -lSDL2 -o ./dist/Project1.exe
+g++ Project1.cpp -std=c++11 -IC:\mingw64\x86_64-w64-mingw32\include\SDL2 -LC:\mingw64\x86_64-w64-mingw32\lib -lmingw32 -lSDL2main -lSDL2 -o ./dist/Project1.exe
 "./dist/Project1.exe"
